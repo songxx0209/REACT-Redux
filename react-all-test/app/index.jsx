7f793424b76954d29f1d4743ead726704da40d71
songxx <913581148@qq.com>
2017-09-28 15:05:38 +0800

    更新

--- react-all-test/app/index.jsx
+++ react-all-test/app/index.jsx
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 // import { Router, Route, IndexRoute, hashHistory, createMemoryHistory } from 'react-router';
 
-import aa from '_aa';
+// import aa from '_aa';
 
 import store from './store';
 import Login from './page/login';
-import styles from './index.less';
+// import styles from './index.less';
 
 
-const app = document.getElementById('app');
 // const history = createMemoryHistory(location);
 // console.log(Provider);
-aa();
+// aa();
 
 ReactDOM.render(
   <Provider store={store}>
     <Login />
   </Provider>,
-app);
+  document.getElementById('app'),
+);
+
--- react-all-test/app/reducers/back.js
+++ react-all-test/app/reducers/back.js
@@ -1,28 +1,26 @@
 
-let initialize = {
+const initialize = {
   three: 'three',
   four: 'four',
-  article: null
-}
+  article: null,
+};
 
 export default function reducer(state = initialize, action) {
-  
   switch (action.type) {
-
     case 'CHANGE_THREE':
-      {
-        return {
-          ...state,
-          three: action.payload,
-        }
-      }
+    {
+      return {
+        ...state,
+        three: action.payload,
+      };
+    }
     case 'CHANGE_FOUR':
-      {
-        return {
-          ...state,
-          four: action.payload,
-        }
-      }
+    {
+      return {
+        ...state,
+        four: action.payload,
+      };
+    }
     // case 'GET_DATA_FULFILLED':
     //   {
     //     console.log('axios==', action.payload);
@@ -33,19 +31,18 @@ export default function reducer(state = initialize, action) {
     //   }
 
     case 'GET_DATA':
-      {
-        console.log('axios==', action.payload);
-        return {
-          ...state,
-          article: action.payload,
-        }
-      }
+    {
+      console.log('axios==', action.payload);
+      return {
+        ...state,
+        article: action.payload,
+      };
+    }
     default:
-      {
-        return {
-          ...state,
-        }
-      }
-      
+    {
+      return {
+        ...state,
+      };
+    }
   }
 }
