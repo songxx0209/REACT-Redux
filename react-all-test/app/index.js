a9796f2b1e93d0ffc9447b6ce44bc597db215fe6
songxx <913581148@qq.com>
2017-09-28 13:53:21 +0800

    更新1

--- react-all-test/app/index.js
+++ react-all-test/app/index.js
@@ -1,15 +1,19 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 // import { Router, Route, IndexRoute, hashHistory, createMemoryHistory } from 'react-router';
+
+import aa from '_aa';
+
 import store from './store';
 import Login from './page/login';
 import styles from './index.less';
 
+
 const app = document.getElementById('app');
 // const history = createMemoryHistory(location);
 // console.log(Provider);
+aa();
 
 ReactDOM.render(
   <Provider store={store}>
